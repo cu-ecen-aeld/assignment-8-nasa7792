829695553660b3392fa31fb90fc7cae527039553
Nalin <nalin@Nalin.myguest.virtualbox.org>
2025-03-16 21:15:38 -0600

    ocal test passed

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION  = 'df3d5179bdf4d18dd9e57eb7600142d5cd04f8e8'
+AESDCHAR_VERSION  = '395f1c2503e2a2cf8688e6b105602c6d351ec179'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -17,7 +17,7 @@ AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/aesdchar
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop.sh $(TARGET_DIR)/etc/init.d/S97aesdchar
 endef
 
 $(eval $(generic-package))
